2cddae202aa57234d27038f600897d18218c42c2
Kent Boogaart <kent.boogaart@gmail.com>
2016-01-30 18:13:48 +1030

    Final tweaks to the deprecated v3.

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
 ".nuget\nuget.exe" "install" "FAKE" "-Version" "3.30.1" "-OutputDirectory" "Src\packages\tools" "-ExcludeVersion"
-"Src\packages\FAKE\tools\Fake.exe" build.fsx
+"Src\packages\FAKE\tools\Fake.exe" build.fsx %*
